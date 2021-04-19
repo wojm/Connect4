07f0c6d03904d213ca69209e607f8a363e2cb200
Matt Wojcieszek <wojo1330@gmail.com>
2021-04-19 17:46:17 -0400

    scope out data model along with 3 tasks

--- src/App.js
+++ src/App.js
@@ -26,6 +26,46 @@ export default function App() {
         <Column></Column>
         <Column></Column>
       </div>
+      <br />
+      It is yellows turn
+      <button> change player</button>
     </div>
   );
 }
+
+/**
+ * Task 1 - show who's turn it is
+ * have a button change whose turn it is
+ */
+
+/**
+ * Task 2 - add functionality for dropping a chip
+ * click on a button above each column
+ *   then the lowest unfilled slot gets filled with a color
+ */
+
+/**
+ * Task 3
+ * show if there are 4 pieces connected in a row
+ *  - vertically
+ *  - horizontally
+ *  - diagonally
+ */
+function isWin(board) {}
+
+const exampleState = {
+  nextTurn: "yellow",
+  board: [[], [], [], ["yellow", "yellow", "red"], ["red"], [], []]
+};
+
+isWin(exampleState.board); // no one wins
+
+isWin([
+  [],
+  [],
+  [],
+  ["yellow", "yellow", "yellow", "yellow", "red"],
+  ["red"],
+  ["red"],
+  ["red"]
+]); // yellow wins
